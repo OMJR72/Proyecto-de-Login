9c5075eeab0e3543e73656a8c640f3d0a13fee3c
Jonatan014 <jonatan014hernandez@gmail.com>
2026-07-08 00:52:37 -0600

    Integración de Bootstrap en login.html

--- js/login.js
+++ js/login.js
@@ -4,20 +4,16 @@ function iniciarSesion(){
     let correo = document.getElementById("correo").value;
     let password = document.getElementById("password").value;
 
-    if(validarCorreo(correo)==false){
+    if(!validarCorreo(correo)){
         document.getElementById("mensajeCorreo").textContent = "Correo inválido";
-        document.getElementById("mensajeCorreo").style.color = "red";
     }
 
-    if(validarPassword(password)==false){
+    if(!validarPassword(password)){
         document.getElementById("mensajePassword").textContent = "La contraseña debe tener mínimo 8 caracteres, una mayúscula, una minúscula, un número y un carácter especial.";
-        document.getElementById("mensajePassword").style.color = "red";
     }
 
     if(validarCorreo(correo)==true && validarPassword(password)==true){
         window.location.href = "index.html";
     }
 }
-document
-    .getElementById("btnLogin")
-    .addEventListener("click", iniciarSesion);
+document.getElementById("btnLogin").addEventListener("click", iniciarSesion);
